c5169f3741c9466c6afc3a4ac5e414b8ec6fc688
suyashgavade <suyashgavade0@gmail.com>
2023-06-30 21:58:12 +0530

    commit

---
+++ ExpressCRUD/app.js
@@ -0,0 +1,17 @@
+const express=require("express")
+const app=express();
+// let path =require("path")
+const bodyparser=require("body-parser");
+const router=require("./router/routers")
+
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
+
+app.use("/",router); 
+
+app.listen(3002,()=>{
+    console.log(" server running at port 3002")
+})
+
+module.exports=app;
+    
